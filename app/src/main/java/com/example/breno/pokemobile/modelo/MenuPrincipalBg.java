0528945ff348d5a1a3eeef6630b73c9fd2ed46f1
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2016-12-01 14:15:25 -0200

    Corrigido mais um bug do bg do menu principal

--- app/src/main/java/com/example/breno/pokemobile/modelo/MenuPrincipalBg.java
+++ app/src/main/java/com/example/breno/pokemobile/modelo/MenuPrincipalBg.java
@@ -9,10 +9,20 @@ import android.media.MediaPlayer;
 
 public class MenuPrincipalBg {
     private static MediaPlayer sound;
+    private static boolean stopped=false;
     public static void playLoop(Context ctx, int raw_id){
-        sound = MediaPlayer.create(ctx, raw_id);
-        sound.setLooping(true); // Set looping
-        sound.start();
+        if(sound == null){
+            sound = MediaPlayer.create(ctx, raw_id);
+            sound.setLooping(true); // Set looping
+            sound.start();
+        }else{
+            if (stopped){
+                sound = MediaPlayer.create(ctx, raw_id);
+                sound.setLooping(true); // Set looping
+                stopped = false;
+            }
+            sound.start();
+        }
     }
     public static boolean isSoundPlaying(){
         if(sound == null){
@@ -22,7 +32,15 @@ public class MenuPrincipalBg {
             return sound.isPlaying();
         }
     }
-    public static void stop(){ sound.stop(); }
+    public static void stop(){ sound.stop(); stopped = true; }
     public static void pause(){ sound.pause(); }
     public static void play() { sound.start(); }
+
+    public static void reset() {
+        sound.reset();
+    }
+
+    public static void release() {
+        sound.release();
+    }
 }
